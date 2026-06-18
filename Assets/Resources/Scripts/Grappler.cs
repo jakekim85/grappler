76a33620d8ff6b42da66544db010add87234f623
jakekim85 <135109241+jakekim85@users.noreply.github.com>
2026-06-18 16:40:00 +0900

    school save

--- Assets/Resources/Scripts/Grappler.cs
+++ Assets/Resources/Scripts/Grappler.cs
@@ -1,27 +1,53 @@
 using System.Xml.Serialization;
+using UnityEditor;
 using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
+    [SerializeField]
+    private bool isColliderInShotRange = false;
+    public bool isSwing=false;
+
+    private Vector3 grabPos;
+
     Vector3 aimPos;
     Camera cam;
     private void Start()
     {
         cam = Camera.main;
-        
-        aimPos = cam.ScreenToWorldPoint(new Vector2(0,0));
     }
     void Update()
     {
-        Debug.DrawRay(cam.transform.position, cam.transform.forward * 20f, Color.red);    
-        if (Input.GetMouseButtonDown(2))
-        {   
-            RaycastHit hit;
+        RaycastHit hit;
 
-            if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20f))
-            {
+        //벽 감지
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20f))
+        {
+            isColliderInShotRange=true;
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * hit.distance, Color.red);
+        }
+        else
+        {
+            isColliderInShotRange=false;
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * 20f, Color.red);
+        }
 
+        //발사시
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (isColliderInShotRange) { 
+
+                if(hit.collider.tag == "Wall")
+                {
+                    grabPos = hit.transform.position;
+
+                    isSwing = true;
+                    Debug.Log("asdfasdfas");
+                }
             }
         }
+
+        
+
     }
 }
